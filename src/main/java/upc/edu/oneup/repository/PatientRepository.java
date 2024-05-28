1bef2656018de5ce0c19ea41381a48d8e3cb6e01
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-05-28 11:03:44 -0500

    subiendo el backend

--- src/main/java/upc/edu/oneup/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package upc.edu.oneup.repository;
-
-import upc.edu.oneup.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
