15653cad6e7a5e480254481e331df1f9f02911e7
Alfredo Farro <48871201+AlfredoFarro@users.noreply.github.com>
2024-06-27 00:03:38 -0500

    Update Product.java

--- src/main/java/upc/edu/oneup/model/Product.java
+++ src/main/java/upc/edu/oneup/model/Product.java
@@ -1,6 +1,7 @@
 package upc.edu.oneup.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -31,11 +32,15 @@ public class Product {
     @Column(name = "product_image_url", nullable = false, length = 250)
     private String productImageUrl;
 
-    @JsonBackReference
+    @JsonBackReference("user-products")
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_user", nullable = false)
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private User user;
 
-}
+    @JsonIgnore
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "payment_method_id", nullable = false)
+    private PaymentMethod paymentMethod;
 
+}
