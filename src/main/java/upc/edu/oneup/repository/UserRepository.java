61a93f0df5ba5c25fda51c6c18fbe1e0d95d065d
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-06-26 21:37:26 -0500

    Update UserRepository.java

--- src/main/java/upc/edu/oneup/repository/UserRepository.java
+++ src/main/java/upc/edu/oneup/repository/UserRepository.java
@@ -4,9 +4,10 @@ import upc.edu.oneup.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>
-{
+public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsername(String username);  // Añadir este método
 }
